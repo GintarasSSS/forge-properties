31704b946cdf0bfd10628bed04cf9df3f175d2e3
GintarasSSS <gintaras_sova@hotmail.com>
2025-06-14 17:23:45 +0100

    Cover more booking scenarios with unit test and add update readme table with them

--- tests/Unit/PropertyRepositoryTest.php
+++ tests/Unit/PropertyRepositoryTest.php
@@ -2,11 +2,12 @@
 
 namespace Tests\Unit;
 
-use App\Models\Property;
-use App\Models\Location;
 use App\Models\Booking;
+use App\Models\Location;
+use App\Models\Property;
 use App\Repositories\PropertyRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 
 class PropertyRepositoryTest extends TestCase
@@ -81,28 +82,8 @@ class PropertyRepositoryTest extends TestCase
         $this->assertFalse($results->contains($prop2));
     }
 
-    public function testFiltersByAvailableFromAndToIncludesPropertiesWithNoBookings()
-    {
-        $property = Property::factory()->create();
-        $bookedProperty = Property::factory()->create();
-
-        Booking::factory()->for($bookedProperty)->create([
-            'date_from' => '2025-07-01',
-            'date_to' => '2025-07-10',
-        ]);
-
-        $repo = new PropertyRepository();
-
-        $results = $repo->search([
-            'available_from' => '2025-07-05',
-            'available_to' => '2025-07-06',
-        ]);
-
-        $this->assertTrue($results->contains($property));
-        $this->assertFalse($results->contains($bookedProperty));
-    }
-
-    public function testFiltersByAvailableFromOnly()
+    #[DataProvider('BookedPropertyDatesNotAvailable')]
+    public function testDoesNotReturnBookedButReturnsNotBookedPropertiesWithSpecificDateFilters(array $filters): void
     {
         $property = Property::factory()->create();
         $booked = Property::factory()->create();
@@ -113,18 +94,55 @@ class PropertyRepositoryTest extends TestCase
         ]);
 
         $repo = new PropertyRepository();
-
-        $results = $repo->search([
-            'available_from' => '2025-07-05',
-        ]);
+        $results = $repo->search($filters);
 
         $this->assertTrue($results->contains($property));
         $this->assertFalse($results->contains($booked));
     }
 
-    public function testFiltersByAvailableToOnly()
+    public static function BookedPropertyDatesNotAvailable(): array
     {
-        $property = Property::factory()->create();
+        return [
+            'available_from within booking dates' => [[
+                'available_from' => '2025-07-05',
+            ]],
+            'available_to within booking dates' => [[
+                'available_to' => '2025-07-07',
+            ]],
+            'available_from and available_to within booking dates' => [[
+                'available_from' => '2025-07-05',
+                'available_to' => '2025-07-06',
+            ]],
+            'available_from and available_to equal to booking dates' => [[
+                'available_from' => '2025-07-01',
+                'available_to' => '2025-07-10',
+            ]],
+            'available_from within and available_to after booking dates' => [[
+                'available_from' => '2025-07-05',
+                'available_to' => '2025-07-15',
+            ]],
+            'available_to equal to booking start date' => [[
+                'available_to' => '2025-07-01'
+            ]],
+            'available_to equal to booking end date' => [[
+                'available_to' => '2025-07-10'
+            ]],
+            'available_from equal to booking start date' => [[
+                'available_from' => '2025-07-01'
+            ]],
+            'available_from equal to booking end date' => [[
+                'available_from' => '2025-07-10'
+            ]],
+            'available_from before and available_to within booking dates' => [[
+                'available_from' => '2025-06-30',
+                'available_to' => '2025-07-05',
+            ]]
+        ];
+    }
+
+    #[DataProvider('bookedPropertyDatesAvailable')]
+    public function testReturnsBookedPropertyWithSpecificDateFilters(array $filters): void
+    {
         $booked = Property::factory()->create();
 
         Booking::factory()->for($booked)->create([
@@ -133,44 +151,69 @@ class PropertyRepositoryTest extends TestCase
         ]);
 
         $repo = new PropertyRepository();
-        $results = $repo->search([
-            'available_to' => '2025-07-07',
-        ]);
 
-        $this->assertTrue($results->contains($property));
-        $this->assertFalse($results->contains($booked));
+        $results = $repo->search($filters);
+
+        $this->assertCount(1, $results);
     }
 
-    public function testReturnsEmptyWhenAllPropertiesAreBooked()
+    public static function bookedPropertyDatesAvailable(): array
     {
-        $booked = Property::factory()->create();
-
-        Booking::factory()->for($booked)->create([
-            'date_from' => '2025-07-01',
-            'date_to' => '2025-07-10',
-        ]);
-
-        $repo = new PropertyRepository();
-
-        $results = $repo->search([
-            'available_from' => '2025-07-05',
-            'available_to' => '2025-07-07',
-        ]);
-
-        $this->assertCount(0, $results);
+        return [
+            'available_to after end of booking' => [[
+                'available_to' => '2025-07-11'
+            ]],
+            'available_to before start of booking' => [[
+                'available_to' => '2025-06-31'
+            ]],
+            'available_from after end of booking' => [[
+                'available_from' => '2025-07-11'
+            ]],
+            'available_from before start of booking' => [[
+                'available_from' => '2025-06-31'
+            ]],
+            'available_from and available_to after end of booking' => [[
+                'available_from' => '2025-07-11',
+                'available_to' => '2025-07-12',
+            ]],
+            'available_from and available_to before start of booking' => [[
+                'available_from' => '2025-06-30',
+                'available_to' => '2025-06-31',
+            ]]
+        ];
     }
 
-    public function testReturnsAllWhenNoBookingsAndNoDateFilters()
+    #[DataProvider('notBookedPropertiesDates')]
+    public function testReturnsAllWhenNoBookingsAndSpecificDateFilters(array $filters): void
     {
         $prop1 = Property::factory()->create();
         $prop2 = Property::factory()->create();
 
         $repo = new PropertyRepository();
 
-        $results = $repo->search([]);
+        $results = $repo->search($filters);
 
         $this->assertTrue($results->contains($prop1));
         $this->assertTrue($results->contains($prop2));
+    }
+
+    public static function notBookedPropertiesDates(): array
+    {
+        return [
+            'available_from only' => [
+                ['available_from' => '2025-07-01']
+            ],
+            'available_to only' => [
+                ['available_to' => '2025-07-10']
+            ],
+            'available_from and available_to' => [
+                [
+                    'available_from' => '2025-07-01',
+                    'available_to' => '2025-07-10'
+                ]
+            ],
+            'no filters' => [[]]
+        ];
     }
 
     public function testDoesNotFailWithNoFilters()
